9d7f81d8a8954c4898025a3e964aeb9210af1397
comp_theme <iraklitsulukidze14@gmail.com>
2023-08-26 10:22:31 +0400

    add history page backend

--- src/main/java/com/independent/springbootlibrary/service/BookService.java
+++ src/main/java/com/independent/springbootlibrary/service/BookService.java
@@ -2,9 +2,16 @@ package com.independent.springbootlibrary.service;
 
 import com.independent.springbootlibrary.dao.BookRepository;
 import com.independent.springbootlibrary.dao.CheckoutRepository;
+import com.independent.springbootlibrary.dao.HistoryRepository;
 import com.independent.springbootlibrary.entity.Book;
 import com.independent.springbootlibrary.entity.Checkout;
+import com.independent.springbootlibrary.entity.History;
 import com.independent.springbootlibrary.responsemodels.ShelfCurrentLoansResponse;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +22,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 @Service
 @Transactional
@@ -24,9 +32,13 @@ public class BookService {
 
     private CheckoutRepository checkoutRepository;
 
-    public BookService(BookRepository bookRepository,CheckoutRepository checkoutRepository){
+    private HistoryRepository historyRepository;
+
+    public BookService(BookRepository bookRepository,CheckoutRepository checkoutRepository,
+                       HistoryRepository historyRepository){
        this.bookRepository=bookRepository;
        this.checkoutRepository=checkoutRepository;
+       this.historyRepository=historyRepository;
     }
 
     public Book checkoutBook (String userEmail, Long bookId) throws Exception{
@@ -87,7 +99,7 @@ public class BookService {
                 TimeUnit time = TimeUnit.DAYS;
 
                 long difference_In_Time = time.convert(
-                        d1.getTime()-d2.getTime(),TimeUnit.MICROSECONDS);
+                        d1.getTime()-d2.getTime(),TimeUnit.MILLISECONDS);
 
                 shelfCurrentLoansResponses.add(new ShelfCurrentLoansResponse(
                         book,(int)difference_In_Time
@@ -97,8 +109,51 @@ public class BookService {
         return shelfCurrentLoansResponses;
     }
 
+    public void returnBook(String userEmail, Long bookId)throws Exception{
+        Optional<Book> book= bookRepository.findById(bookId);
+
+        Checkout validateCheckout = checkoutRepository.findByUserEmailAndBookId(userEmail, bookId);
+
+        if(!book.isPresent()||validateCheckout == null){
+            throw new Exception("book doesn't exist or notcheckedout  by user");
+        }
+
+        book.get().setCopiesAvailable(book.get().getCopiesAvailable()+1);
+
+        bookRepository.save(book.get());
+
+        checkoutRepository.deleteById(validateCheckout.getId());
+
+        History history = new History(
+              userEmail,
+                validateCheckout.getCheckoutDate(),
+                LocalDate.now().toString(),
+                book.get().getTitle(),
+                book.get().getAuthor(),
+                book.get().getDescription(),
+                book.get().getImg()
+        );
+        historyRepository.save(history);
 
 
 
+    }
+
+    public void renewLoan(String userEmail, Long bookId)throws Exception{
+        Checkout validateCheckout = checkoutRepository.findByUserEmailAndBookId(userEmail, bookId);
+        if(validateCheckout ==null){
+            throw new Exception("Book does not exist or not checked out by user");
+        }
+        SimpleDateFormat sdFormat= new SimpleDateFormat("yyyy-MM-dd");
+
+        Date d1 =sdFormat.parse(validateCheckout.getReturnDate());
+        Date d2 =sdFormat.parse(LocalDate.now().toString());
+
+        if(d1.compareTo(d2)>0 ||d1.compareTo(d2) ==0){
+            validateCheckout.setReturnDate(LocalDate.now().plusDays(7).toString());
+            checkoutRepository.save(validateCheckout);
+        }
+
+    }
 
 }
